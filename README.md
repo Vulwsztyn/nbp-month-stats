afd120b424a932c8f009d653dc8078e27c334455
Artur Mostowski <artur.mostowski@protonmail.com>
2022-04-28 13:28:14 +0200

    initial version of the script

---
+++ README.md
@@ -0,0 +1,7 @@
+# NBP month stats
+Look for the lowest price of a particular currency in particular month
+### Usage
+1. Install ruby
+2. `gem install bundler`
+3. `bundle install`
+4. `ruby main.rb currency month year` e.g. `ruby main.rb USD 4 22` - all params are optional and defualt to USD and current month
---
+++ main.rb
@@ -0,0 +1,33 @@
+require 'date'
+require 'pry'
+require 'httparty'
+require 'hash_dot'
+
+Hash.use_dot_syntax = true
+Hash.hash_dot_use_default = true
+
+def process_response(response, currency)
+  data = response.map do |day|
+    [day.effectiveDate, day.rates.find {|x| x.code == currency}.mid]
+  end.sort_by {|x| x[1]}
+  puts data.slice(0,10).map {|x| "#{x[0]} #{x[1]}"}.join("\n")
+end
+
+def main
+    currency = ARGV.length > 0 ? ARGV[0] : 'USD'
+    month = ARGV.length > 1 ? ARGV[1].to_i : Date.today.month
+    year = ARGV.length > 2 ? 2000+ARGV[2].to_i : Date.today.year
+    
+    start_date = Date.new(year, month, 1)
+    end_date = Date.new(year, month, -1) > Date.today ? Date.today : Date.new(year, month, -1)
+
+    if start_date > Date.today
+        puts 'No future'
+        return
+    end
+    puts "Looking for #{currency} rates from #{start_date.to_s} to #{end_date.to_s}:"
+    response = HTTParty.get("http://api.nbp.pl/api/exchangerates/tables/A/#{start_date.to_s}/#{end_date.to_s}/")
+    process_response response, currency
+end
+
+main
